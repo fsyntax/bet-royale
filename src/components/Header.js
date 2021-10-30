1822387234923a7b5a1d36ade7872930bfed7a2a
warbaddy19 <warbaddy19@gmail.com>
2021-10-30 16:28:28 -0300

    Did things I do not understand

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,32 @@ import discord from "../images/discord.svg";
 const Header = () => {
   const location = useLocation();
 
+  window.onload = () => {
+    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const [accessToken, tokenType] = [
+      fragment.get("access_token"),
+      fragment.get("token_type"),
+    ];
+
+    if (!accessToken) {
+      return (document.getElementById("login").style.display = "block");
+    }
+
+    fetch("https://discord.com/api/users/@me", {
+      headers: {
+        authorization: `${tokenType} ${accessToken}`,
+      },
+    })
+      .then((result) => result.json())
+      .then((response) => {
+        const { username, discriminator } = response;
+        document.getElementById(
+          "info"
+        ).innerText += ` ${username}#${discriminator}`;
+      })
+      .catch(console.error);
+  };
+
   return (
     <header className="flex justify-between items-center w-full p-5 mb-10">
       <div>
@@ -40,11 +66,13 @@ const Header = () => {
           className="outline-none rounded bg-white text-black p-3 flex justify-center items-center"
           target="_blank"
           rel="noreferrer"
-          href="https://cryptoroyale.one/ranked/auth/discord/redirect"
+          id="login"
+          href="https://discord.com/api/oauth2/authorize?client_id=903764073966096425&redirect_uri=http%3A%2F%2Flocalhost%3A5000&response_type=code&scope=identify"
         >
           Link With Discord!
           <img className="w-8 ml-3" src={discord} alt="discord" />
         </a>
+        <div id="info"></div>
       </div>
     </header>
   );
--- src/server.js
+++ src/server.js
@@ -1,79 +1,12 @@
-var express = require("express"),
-  session = require("express-session"),
-  passport = require("passport"),
-  Strategy = require("./lib").Strategy,
-  app = express();
+const express = require("express");
+const { port } = require("./config.json");
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.render("../public/index.html", { root: "." });
 });
 
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
-
-var scopes = ["identify", "email"];
-
-var prompt = "consent";
-
-passport.use(
-  new Strategy(
-    {
-      clientID: "903764073966096425",
-      clientSecret: "ScMiEZkl-Q_vIDexXM4QIge5TSf-XTcM",
-      callbackURL: "http://localhost:5000/callback",
-      scope: scopes,
-      prompt: prompt,
-    },
-    function (accessToken, refreshToken, profile, done) {
-      process.nextTick(function () {
-        return done(null, profile);
-      });
-    }
-  )
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
 );
-
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-app.use(passport.initialize());
-
-app.use(passport.session());
-
-app.get(
-  "/",
-  passport.authenticate("discord", { scope: scopes, prompt: prompt }),
-  function (req, res) {}
-);
-
-app.get(
-  "/callback",
-  passport.authenticate("discord", { failureRedirect: "/" }),
-  function (req, res) {
-    res.redirect("/info");
-  } // auth success
-);
-
-app.get("/logout", function (req, res) {
-  req.logout();
-  res.redirect("/");
-});
-
-app.get("/info", checkAuth, function (req, res) {
-  res.json(req.user);
-});
-
-function checkAuth(req, res, next) {
-  if (req.isAuthenticated()) return next();
-  res.send("not logged in");
-}
-
-app.listen(5000, function (err) {
-  if (err) return console.log(err);
-  console.log("Listening at http://localhost:5000/");
-});
