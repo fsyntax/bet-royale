5a190cde773cef34050eb828576a318896aa950f
FreibergerSyntax <fsyntax@protonmail.com>
2021-11-19 14:08:53 +0100

    Added SCSS to the project, Started working on the Header/Navbar Layout/responsivness; Added a basic Bootstrap Navbar as a Boilerplate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Modal } from "react-bootstrap";
+import { Modal, Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 
 import logo from "../images/logo.png";
 import discord from "../images/discord.svg";
 
-const Header = () => {
+const Header = (props) => {
   const location = useLocation();
 
   const [modal, setModal] = useState(false);
   const [description, setDescription] = useState("");
+
 
   function connectToWallet() {
     if (window.ethereum) {
@@ -61,7 +62,7 @@ const Header = () => {
   }
 
   return (
-    <div className="w-100 mt-4">
+    <div className="header-wrapper w-100">
       <Modal show={modal}>
         <div className="modal-content">
           <div className="modal-header">
@@ -88,102 +89,96 @@ const Header = () => {
           </div>
         </div>
       </Modal>
-      <header
-        className="container d-flex justify-content-between align-items-center w-100 h-100 p-5 mb-10"
-        style={{
-          fontSize: "15px",
-        }}
-      >
-        <div className="d-flex justify-content-start">
-          <img
-            style={{ height: "50px" }}
-            className="img-fluid"
-            src={logo}
-            alt="logo"
-          />
-        </div>
-        <div className="d-flex justify-content-end align-items-center w-100">
-          <Link
-            className={
-              location.pathname === "/"
-                ? "pr-3 text-danger text-decoration-none"
-                : "pr-3 text-white text-decoration-none"
-            }
-            style={{ marginRight: "15px", outline: "none" }}
-            to="/"
-          >
-            Home
-          </Link>
-          <Link
-            className={
-              location.pathname === "/createBet"
-                ? "pr-3 text-danger text-decoration-none"
-                : "pr-3 text-white text-decoration-none"
-            }
-            style={{ marginRight: "15px", outline: "none" }}
-            to="/createBet"
-          >
-            Create Bet
-          </Link>
-          <Link
-            className={
-              location.pathname === "/bettingHistory"
-                ? "pr-3 text-danger text-decoration-none"
-                : "pr-3 text-white text-decoration-none"
-            }
-            style={{ marginRight: "15px", outline: "none" }}
-            to="/bettingHistory"
-          >
-            Betting History
-          </Link>
 
-          {localStorage.getItem("address") && (
-            <div
-              className="ml-3 d-flex flex-column"
-              style={{ marginLeft: "15px" }}
-            >
-              <button className="btn btn-secondary">
-                Address: {localStorage.getItem("shortenedAddress")}
-              </button>
-            </div>
-          )}
-          {!localStorage.getItem("address") && (
-            <div style={{ marginLeft: "15px" }}>
-              <button
-                onClick={connectToWallet}
-                className="btn btn-success ml-3"
+      <Navbar className="main-navbar" collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+        <Container>
+          <Navbar.Brand href="#home" className="header-logo--wrapper">
+            <img
+              style={{ height: "50px" }}
+              className="header-logo img-fluid"
+              src={logo}
+              alt="logo"
+            />
+            <span className="header-logo--text">
+              BetRoyale
+            </span>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="main-navbar-nav" />
+          <Navbar.Collapse id="main-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link className={
+                location.pathname === "/"
+                  ? "pr-3 text-danger text-decoration-none"
+                  : "pr-3 text-white text-decoration-none"
+              }
+                to="/"
               >
-                Connect to a Wallet
-              </button>
-            </div>
-          )}
-          {localStorage.getItem("username") && (
-            <div style={{ marginLeft: "15px" }}>
-              <button className="btn btn-primary">
-                {localStorage.getItem("username")}
-              </button>
-            </div>
-          )}
-          {!localStorage.getItem("username") && (
-            <a
-              className="text-decoration-none btn btn-success d-flex justify-items-center align-items-center"
-              style={{ marginLeft: "15px" }}
-              target="_blank"
-              rel="noreferrer"
-              id="login"
-              href="https://discord.com/api/oauth2/authorize?client_id=903764073966096425&redirect_uri=https%3A%2F%2Fbet-royale.netlify.app%2F&response_type=token&scope=identify"
-            >
-              Link With Discord!
-              <img
-                className="ml-3 d-inline img-fluid"
-                style={{ height: "25px", marginLeft: "15px" }}
-                src={discord}
-                alt="discord"
-              />
-            </a>
-          )}
-        </div>
-      </header>
+                Home</Nav.Link>
+              <Nav.Link className={
+                location.pathname === "/createBet"
+                  ? "pr-3 text-danger text-decoration-none"
+                  : "pr-3 text-white text-decoration-none"
+              }
+                to="/createBet"
+              >
+                Create Bet</Nav.Link>
+              <Nav.Link className={
+                location.pathname === "/bettingHistory"
+                  ? "pr-3 text-danger text-decoration-none"
+                  : "pr-3 text-white text-decoration-none"
+              }
+                to="/bettingHistory"
+              >
+                Betting History</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Item >
+                {localStorage.getItem("address") && (
+                  <div
+                    className="ml-3 d-flex flex-column"
+                    style={{ marginLeft: "15px" }}
+                  >
+                    <button className="btn btn-secondary">
+                      Address: {localStorage.getItem("shortenedAddress")}
+                    </button>
+                  </div>
+                )}
+                {!localStorage.getItem("address") && (
+                  <div style={{ marginLeft: "15px" }}>
+                    <button
+                      onClick={connectToWallet}
+                      className="btn btn-success ml-3"
+                    >
+                      Connect to a Wallet
+                    </button>
+                  </div>
+                )}
+                {localStorage.getItem("username") && (
+                  <div style={{ marginLeft: "15px" }}>
+                    <button className="btn btn-primary">
+                      {localStorage.getItem("username")}
+                    </button>
+                  </div>
+                )}</Nav.Item>
+              {!localStorage.getItem("username") && (
+                <Nav.Link className="text-decoration-none btn btn-success d-flex justify-items-center align-items-center"
+                  target="_blank"
+                  rel="noreferrer"
+                  id="login" href="https://discord.com/api/oauth2/authorize?client_id=903764073966096425&redirect_uri=https%3A%2F%2Fbet-royale.netlify.app%2F&response_type=token&scope=identify">
+
+                  Link With Discord!
+                  <img
+                    className="ml-3 d-inline img-fluid"
+                    style={{ height: "25px", marginLeft: "15px" }}
+                    src={discord}
+                    alt="discord"
+                  />
+                </Nav.Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
