38050bc66a0a78178f129e6b1276e252dc3b7b65
FreibergerSyntax <fsyntax@protonmail.com>
2021-11-22 19:00:57 +0100

    Fixed merging conflict

--- src/components/BettingTable.js
+++ src/components/BettingTable.js
@@ -12,10 +12,14 @@ import ToastContainer from "react-bootstrap/ToastContainer";
 
 const BettingTable = (props) => {
   const [betState, setBetState] = useState([]);
+  const [betData, setBetData] = useState();
   const [descriptionModal, setDescriptionModal] = useState(false);
   const [betDeleteModal, setBetDeleteModal] = useState(false);
   const [metamaskModal, setMetamaskModal] = useState(false);
   const [betToast, setBetToast] = useState(false);
+  const [betOptionModal, setBetOptionModal] = useState(false);
+  const [betOptions, setBetOptions] = useState("");
+  const [selectedBetOption, setSelectedBetOption] = useState("");
   const [betToastDescription, setBetToastDescription] = useState("");
   const [description, setDescription] = useState("");
   const [filteredBets, setFilteredBets] = useState([]);
@@ -43,18 +47,6 @@ const BettingTable = (props) => {
 
   async function handleBet({ amount, address, objData }) {
     try {
-      if (!window.ethereum) {
-        setMetamaskModal(true);
-        setDescription("Please install Metamask!");
-        return;
-      }
-
-      if (!localStorage.getItem("address")) {
-        setMetamaskModal(true);
-        setDescription("Please connect your wallet!");
-        return;
-      }
-
       await window.ethereum.send("eth_requestAccounts");
 
       const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -68,12 +60,18 @@ const BettingTable = (props) => {
         value: ethers.utils.parseEther(amount),
       });
 
+      setBetOptionModal(false);
+      setBetOptions("");
+      setSelectedBetOption("");
+
       setBetToast(true);
       setBetToastDescription("This bet has been successfully placed!");
 
       setBetState([...betState, objData.id]);
 
       BetService.getInstance().logBet(objData);
+
+      setBetData();
     } catch (error) {
       console.clear();
       setBetToast(true);
@@ -99,8 +97,6 @@ const BettingTable = (props) => {
   }
 
   function deleteCurrentBet(id, name) {
-    console.log(id);
-
     setBetDeleteModal(true);
     setDescription(`Are you sure you want to delete '${name}'?`);
 
@@ -111,6 +107,44 @@ const BettingTable = (props) => {
 
   function closeBetToast() {
     setBetToast(false);
+  }
+
+  function closeBetOptionModal() {
+    setBetOptionModal(false);
+  }
+
+  function openBetOptionModal(bet) {
+    if (!window.ethereum) {
+      setMetamaskModal(true);
+      setDescription("Please install Metamask!");
+      return;
+    }
+
+    if (!localStorage.getItem("address")) {
+      setMetamaskModal(true);
+      setDescription("Please connect your wallet!");
+      return;
+    }
+
+    setBetData(bet);
+
+    let newOptions = bet.choices.split(",");
+
+    setBetOptionModal(true);
+    setBetOptions(newOptions);
+  }
+
+  function changeBetOption(e) {
+    setSelectedBetOption(e.target.value);
+  }
+
+  function betOnOption(bet) {
+    if (selectedBetOption !== "") {
+      bet.selectedOption = selectedBetOption;
+      placeBet(bet);
+    } else {
+      return;
+    }
   }
 
   return (
@@ -137,6 +171,44 @@ const BettingTable = (props) => {
               onClick={closeDescriptionModal}
             >
               Close
+            </button>
+          </div>
+        </div>
+      </Modal>
+      <Modal show={betOptionModal}>
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Options</h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              onClick={closeBetOptionModal}
+            ></button>
+          </div>
+          <div className="modal-body">
+            <select
+              className="form-select"
+              onChange={changeBetOption}
+              name=""
+              id=""
+            >
+              {betOptions &&
+                betOptions.map((option) => (
+                  <option key={option} value={option} defaultValue>
+                    {option}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={() => betOnOption(betData)}
+            >
+              Bet
             </button>
           </div>
         </div>
@@ -288,7 +360,7 @@ const BettingTable = (props) => {
                 {!betState.includes(currentBet.id) && (
                   <button
                     className="outline-none btn"
-                    onClick={() => placeBet(currentBet)}
+                    onClick={() => openBetOptionModal(currentBet)}
                   >
                     Place Bet
                   </button>
