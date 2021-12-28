0a2a16c2a3328cfa6acfc60894697635e4f686cc
FreibergerSyntax <fsyntax@protonmail.com>
2021-12-28 22:48:46 +0100

    Bet History fix

--- src/components/BetHistoryTable.js
+++ src/components/BetHistoryTable.js
@@ -1,10 +1,31 @@
 import moment from "moment";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const BetHistoryTable = (props) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+      setIsLoaded(true);
+  }, []);
+  const variants = {
+    loaded: {
+      opacity: 1, 
+      transition: {
+        duration: .7,
+        ease: "backIn"
+      } 
+    },
+    notLoaded: {opacity: 0}
+  }
   return (
     <div className="container betting-table w-100">
       {props.data.map((currentBet, index) => (
-        <div className="betting-table__bet" key={currentBet.id} index={index}>
+          <motion.div 
+          className="betting-table__bet" 
+          key={currentBet.id} 
+          index={index} 
+          animate={isLoaded ? "loaded" : "notLoaded"}
+          variants={variants} >  
           <div className="betting-table__bet__header">
             <h3 className="betting-table__bet__name">{currentBet.name}</h3>
           </div>
@@ -36,7 +57,7 @@ const BetHistoryTable = (props) => {
               </ul>
             </div>
           </div>
-        </div>
+          </motion.div>
       ))}
     </div>
   );
